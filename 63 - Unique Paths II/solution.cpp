53bcb89ad7a840cd5ac3596c5ada716adbe8e24a
LingSung <tumi9325@gmail.com>
2022-05-20 09:26:55 +0800

    Finish LeetCode 63 - Unique Paths II

---
+++ 63 - Unique Paths II/solution.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int n = obstacleGrid.size(), m = obstacleGrid[0].size();
+        vector<vector<int>> dp(n+1, vector<int>(m+1, 0));
+        dp[1][0] = 1;
+        for (int i=1; i<=n; i++) {
+            for (int j=1; j<=m; j++) {
+                if (obstacleGrid[i-1][j-1] == 0) dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[n][m];
+    }
+};
