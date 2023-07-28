f3d6a06e00ecf308e76a7da92a3151df3310a715
zero1777 <tumi9325@gmail.com>
2023-07-28 14:05:02 +0800

    Finish LeetCode 300 - Longest Increasing Subsequence

--- 300 - Longest Increasing Subsequence/solution.cpp
+++ 300 - Longest Increasing Subsequence/solution.cpp
@@ -1,3 +1,4 @@
+// Recursion + Memoization
 class Solution {
 public:
     int maxLength = 1;
@@ -23,4 +24,26 @@ public:
 
         return maxLength;
     }
-}
+};
+
+// DP
+class Solution {
+public:
+    int lis(vector<int>& nums) {
+        int n = nums.size();
+        int maxLength = 1;
+        vector<int> dp(n, 1);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) dp[i] = max(dp[i], 1 + dp[j]);
+            }
+            maxLength = max(maxLength, dp[i]);
+        }
+
+        return maxLength;
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        return lis(nums);
+    }
+};
